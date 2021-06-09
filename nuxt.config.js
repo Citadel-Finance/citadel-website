9bf793f4124544f14661229d3e791305a39dcfec
valerialoskutova <valeriia.loskutova@yandex.ru>
2021-06-09 18:11:08 +0700

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,11 @@ export default {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap',
       },
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+        type: 'image/x-icon',
+      },
     ],
   },
   css: [
